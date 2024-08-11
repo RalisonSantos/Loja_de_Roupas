f348aecf3fb4a6c976d32f2550146bef36e0dbab
ralisonsantos <ralisonruanv@gmail.com>
2024-08-10 23:29:28 -0300

    home modificada

--- codigo/js/pacote.js
+++ codigo/js/pacote.js
@@ -1,7 +1,20 @@
-const carrossel = document.querySelector(".carrossel");
+const slide = document.querySelector(".slide"),
+proximaimg = document.querySelectorAll("img")[0],
+mudandoimg = document.querySelectorAll(".pacote i");
 
-const arrastar = (e) =>{
-    carrossel.scrollLeft = e.pageX;
+let isDragStart = false , prevPageX, prevScrollLeft;
+let modfbtn = proximaimg.clientWidth + 15;
+let scrollWidth = slide.scrollWidth - slide.clientWidth;
+
+const mostrabtnsq = () => {
+    mudandoimg[0].style.display = slide.scrollLeft == 0 ? "none" : "block";
+    mudandoimg[1].style.display = slide.scrollLeft == scrollWidth ? "none" : "block";
 }
 
-carrossel.addEventListener("movermouse", arrastar);
+mudandoimg.forEach(icon => {
+    icon.addEventListener("click", () => {
+        slide.scrollLeft += icon.id == "left" ? -modfbtn : modfbtn;
+        mostrabtnsq();
+    });
+});
+
