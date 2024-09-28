f6efe51dba20deffcdc5fe8accb56a2efd6f5676
RalisonSantos <ralisonruanv@gmail.com>
2024-09-28 20:03:02 -0300

    Adicionada barra de opções

--- src/js/opcoes.js
+++ src/js/opcoes.js
@@ -1,3 +1,11 @@
 function listagem_opcoes() {
     document.getElementById("lopcoes").classList.toggle("ativado");
 }
+
+function opcoes_usuario() {
+    document.getElementById("usuario").classList.toggle("atv_usuario");
+}
+
+function opcoes_fornecedor() {
+    document.getElementById("fornecedor").classList.toggle("atv_fornecedor");
+}
